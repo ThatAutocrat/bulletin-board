da6805ba417643f43f5149c2efdb6abe9f95a4f4
Devashish Gogoi <hellonat3456@gmail.com>
2026-05-24 12:45:53 +0530

    Update db.go

--- internal/models/db.go
+++ internal/models/db.go
@@ -2,19 +2,34 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
-	_ "modernc.org/sqlite"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 func InitDB(dbPath string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite", dbPath+"?_foreign_keys=on")
+	tursoURL := os.Getenv("TURSO_URL")
+	tursoToken := os.Getenv("TURSO_TOKEN")
+
+	var dsn string
+	if tursoURL != "" && tursoToken != "" {
+		dsn = fmt.Sprintf("%s?authToken=%s", tursoURL, tursoToken)
+	} else if dbPath != "" {
+		// Local dev fallback: use a local file via libsql
+		dsn = fmt.Sprintf("file:%s", dbPath)
+	} else {
+		dsn = "file:./bulletin.db"
+	}
+
+	db, err := sql.Open("libsql", dsn)
 	if err != nil {
 		return nil, err
 	}
 	if err := db.Ping(); err != nil {
 		return nil, err
 	}
+
 	schema, err := os.ReadFile("db/schema.sql")
 	if err != nil {
 		return nil, err
